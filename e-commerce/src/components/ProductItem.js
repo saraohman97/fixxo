db0b51b08c6b3379074bc037b267d5f97c602a79
saraohman97 <saraohman97@gmail.com>
2022-10-09 02:15:36 +0200

    Completely done with Featured Product by adding hover function.

--- e-commerce/src/components/ProductItem.js
+++ e-commerce/src/components/ProductItem.js
@@ -5,24 +5,27 @@ import star from '../img/star.svg'
 const ProductItem = () => {
   return (
     <div className='product-item'>
+
       <div className="quick-view">QUICK VIEW</div>  
       <div className="featured-icons">
         <i className="fa-solid fa-code-compare"></i>
         <i className="fa-regular fa-heart"></i>
         <i className="fa-solid fa-bag-shopping"></i>
       </div>
-        <img src={grayBox} className='gray-box' alt="" />
-        <div className="text">
-            <small className='clr-gray'>Category</small>
-            <strong className='not-so-strong'>Modern Black Blouse</strong>
-            <div className="stars">
-                <img src={star} alt="" />
-                <img src={star} alt="" />
-                <img src={star} alt="" />
-                <img src={star} alt="" />
-                <img src={star} alt="" />
-            </div>
-            <div>$30.00</div>
+      <div className="layer"></div>
+
+      <img src={grayBox} className='gray-box' alt="" />
+      <div className="text">
+          <small className='clr-gray'>Category</small>
+          <strong className='not-so-strong'>Modern Black Blouse</strong>
+          <div className="stars">
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+              <img src={star} alt="" />
+          </div>
+          <div>$30.00</div>
         </div>
     </div>
   )
