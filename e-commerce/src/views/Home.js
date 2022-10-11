5b31033945315d33a6011f9c845be91c589f4f77
unknown <emil.lind@utb.ecutbildning.se>
2022-10-11 09:26:41 +0200

    flash showcase done

--- e-commerce/src/views/Home.js
+++ e-commerce/src/views/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import FlashSaleShowcase from '../components/FlashSaleShowcase'
 import ProductSales from '../components/ProductSales'
 import '../styles/Home.css'
 
@@ -6,6 +7,7 @@ const Home = () => {
   return (
     <div>
       <ProductSales />
+      <FlashSaleShowcase />
     </div>
   )
 }
