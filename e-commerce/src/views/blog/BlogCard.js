191d9ffcbe96fed4ddbf276e0a52755eb54ff505
Birukmengistu <sheger.shelf3@gmail.com>
2022-10-27 08:31:09 +0200

    added json -backend

--- e-commerce/src/views/blog/BlogCard.js
+++ e-commerce/src/views/blog/BlogCard.js
@@ -6,14 +6,14 @@ import {Link} from 'react-router-dom'
 * @Blog function
 **/
 
-const BlogCard = () => {
+const BlogCard = ({blog}) => {
   return(
     <div className='blogcard'>
        <Link to ='/blogdetails'>
-          <img src={'https://images.unsplash.com/photo-1665596553794-61961a0b40ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxODN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60'} className='blogcard-img' alt='img-blog'/>
-                </Link>
+          <img src={blog.image} className='blogcard-img' alt='img-blog'/>
+        </Link>
         <p>
-          Blog-content-Title
+          {blog.blogBody}
         </p>
        
        
--- e-commerce/src/views/blog/BlogPage.js
+++ e-commerce/src/views/blog/BlogPage.js
@@ -1,33 +1,44 @@
-import React from 'react'
+import React,{ useState ,useEffect} from 'react'
+import axios from 'axios'
 import BlogCard  from './BlogCard'
 import './blog.css'
 import './blogcard-styles.css'
 import BlogCategories  from './BlogCategories'
+
 /**
 * @Biruk
 * @Blog-page function
 **/
 
 const BlogPage = () => {
-  return(
+  const [blogCards, setBlogCards] =  useState();
+  useEffect(() => {
+    getAllBlogs()
+  }, [])
+
+
+  const getAllBlogs = async () => {
+    const blogs = await axios.get("http://localhost:9999/blog")
+    setBlogCards(blogs.data)
+     console.log(blogs.data)
+     }
+  return (
     <div className='blog-details-layout'>
       
       <div className='blogcard-container'>
-      <BlogCard/>
-      <BlogCard/>
-      <BlogCard/>
-      <BlogCard/>
-      <BlogCard/>
-      <BlogCard/>
-      <BlogCard/>
-      <BlogCard/>
+      {blogCards ? blogCards.map((blog) => (
+              <BlogCard key={blog.id} blog={blog} />
+            )) : <div></div>}
+      
+         
       </div>
       <div className='blog-side-categories'>
-       <BlogCategories/>
-        </div>
+          <BlogCategories/>
+      </div>
     </div>
    )
 
  }
+
 
  export default BlogPage
